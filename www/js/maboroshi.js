c2eb7bd77284000377b1e9b1ad4bc57b1a782db2
John Mettraux <jmettraux@gmail.com>
2021-12-12 15:36:49 +0900

    Make first spec succeed

--- www/js/maboroshi.js
+++ www/js/maboroshi.js
@@ -15,6 +15,18 @@ var Dice = (function() {
   // public functions
 
   this.parse = function(s) {
+
+    var s1 = '' + s;
+    var a = [];
+
+    while(s1.length > 0) {
+      var m = s1.match(/^(\d+)d(\d+)(.*)$/);
+      if ( ! m) break;
+      a.push({ c: parseInt(m[1], 10), d: parseInt(m[2], 10) });
+      s1 = m[3];
+    }
+
+    return a;
   };
 
   this.roll = function(s) {
