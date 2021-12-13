3488e87ddb82e7dffcdb69032afc49c73ea38a84
John Mettraux <jmettraux@gmail.com>
2021-12-14 08:55:54 +0900

    Parse dice plus and numbers

--- www/js/maboroshi.js
+++ www/js/maboroshi.js
@@ -14,12 +14,28 @@ var Dice = (function() {
 
   var parseDice = function(s) {
 
-    var m = s.match(/^(\d*)d(\d+)(.*)$/);
+    var m = s.match(/^\s*(\d*)d(\d+)(.*)$/);
     if ( ! m) return null;
     var c = m[1]; c = (c === '') ? 1 : parseInt(m[1], 10);
     var d = parseInt(m[2], 10);
 
     return [ { c: c, d: d }, m[m.length - 1] ];
+  };
+
+  var parseOperation = function(s) {
+
+    var m = s.match(/^\s*(\+)\s*(.*)$/);
+    if ( ! m) return null;
+
+    return [ m[1], m[m.length - 1] ];
+  };
+
+  var parseNumber = function(s) {
+
+    var m = s.match(/^\s*(\d+)\s*(.*)$/);
+    if ( ! m) return null;
+
+    return [ parseInt(m[1], 10), m[m.length - 1] ];
   };
 
   // public functions
@@ -31,7 +47,7 @@ var Dice = (function() {
 
     while(s1.length > 0) {
 
-      var r = parseDice(s1);
+      var r = parseDice(s1) || parseOperation(s1) || parseNumber(s1);
       if ( ! r) break;
 
       a.push(r[0]);
